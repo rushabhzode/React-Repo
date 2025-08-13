f27dabbfdbf1a1f369ceb55c14c62c5a93ba4234
rushabh <rushabhzode84@gmail.com>
2025-08-13 16:31:20 +0530

    update app.jsx and main.jsx file

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
     <h1>Vite react app</h1>
-    <Youtube />
+    <Youtube />   
     </>
   )
 }
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -2,8 +2,31 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+const reactElement ={
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target:'_blank',
+
+},
+    children: ("click me to visit google")
+}
+
+function MyApp() {
+    return (
+         <div>
+            <h1>Custom React App</h1>
+         </div>
+    )
+}
+
+const anotherElement = (
+    <a href="https://google.com" target="_blank">visit google</a>
+)
+
+
 createRoot(document.getElementById('root')).render(
  
-    <App />
+  anotherElement
 
 )
